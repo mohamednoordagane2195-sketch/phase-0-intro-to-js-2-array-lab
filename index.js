a89dbd804b655572fa0b44ff0763287b84478f00
Mohamed Noor <mohamednoordagane2195@gmail.com>
2026-01-18 23:07:47 +0300

    welcome

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+function destructivelyAppendCat(name) {
+  cats.push(name);
+  return cats;
+}
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+  return cats;
+}
+function destructivelyRemoveLastCat() {
+  cats.pop();
+  return cats;
+}
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+  return cats;
+}
+function appendCat(name) {
+  return [...cats, name];
+}
+function prependCat(name) {
+  return [name, ...cats];
+}
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+function removeFirstCat() {
+  return cats.slice(1);
+}
+
+var cats = ["Milo", "Otis", "Garfield"];
+
+
+
+
